76a2d57ef343716c151f88fe160d82b8eca38d95
Jan Florian Dietrich <luhmann@users.noreply.github.com>
2018-03-18 09:14:38 +0100

    test(e2e): switch to uat-form

--- cypress/integration/home/routing_spec.js
+++ cypress/integration/home/routing_spec.js
@@ -1,51 +1,53 @@
 import { sel } from '../../support/utils';
 
-describe('GIVEN a search-term set in the route', () => {
-  beforeEach(() => {
-    cy.server();
-
-    cy.route({
-      method: 'GET',
-      url: /https:\/\/api.punkapi.com\/v2/,
-      response: 'fixture:beers-successful.json',
-    });
-
-    cy.visit('/#Burger');
-  });
-
-  it('should show a list of results', () => {
-    cy.get(sel('beer-card')).should('have.length', 14);
-  });
-
-  describe('WHEN searching again', () => {
+describe('Routing', () => {
+  describe('GIVEN a search-term set in the route', () => {
     beforeEach(() => {
-      cy
-        .get(sel('search-box'))
-        .clear()
-        .type('Steak')
-        .should('have.value', 'Steak')
-        .type('{enter}');
+      cy.server();
+
+      cy.route({
+        method: 'GET',
+        url: /https:\/\/api.punkapi.com\/v2/,
+        response: 'fixture:beers-successful.json',
+      });
+
+      cy.visit('/#Burger');
     });
 
-    it('should change the url', () => {
-      cy.url().should('contain', '/#Steak');
+    it('should show a list of results', () => {
+      cy.get(sel('beer-card')).should('have.length', 14);
     });
 
-    describe('AND hitting the browsers `back`-button', () => {
+    describe('WHEN searching again', () => {
       beforeEach(() => {
-        cy.go('back');
+        cy
+          .get(sel('search-box'))
+          .clear()
+          .type('Steak')
+          .should('have.value', 'Steak')
+          .type('{enter}');
       });
 
-      it('should set the original url', () => {
-        cy.url().should('contain', '/#Burger');
+      it('should change the url', () => {
+        cy.url().should('contain', '/#Steak');
       });
 
-      describe('AND hitting the browsers `forward`-button', () => {
+      describe('AND hitting the browsers `back`-button', () => {
         beforeEach(() => {
-          cy.go('forward');
+          cy.go('back');
         });
-        it('should set the next-url', () => {
-          cy.url().should('contain', '/#Steak');
+
+        it('should set the original url', () => {
+          cy.url().should('contain', '/#Burger');
+        });
+
+        describe('AND hitting the browsers `forward`-button', () => {
+          beforeEach(() => {
+            cy.go('forward');
+          });
+          it('should set the next-url', () => {
+            cy.url().should('contain', '/#Steak');
+          });
         });
       });
     });
--- cypress/integration/home/search_beers_spec.js
+++ cypress/integration/home/search_beers_spec.js
@@ -1,43 +1,49 @@
 import { sel } from '../../support/utils';
 
-describe('searching for beers', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
+describe('Searching for beers', () => {
+  describe('GIVEN the initial page', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
 
-  describe('with entering text in the search-field', () => {
-    describe('with real backend', () => {
-      it('should resolve the request and display a list of beers', () => {
+    describe('AND the real backend', () => {
+      beforeEach(() => {
         cy.server();
 
         cy.route(/https:\/\/api.punkapi.com\/v2/).as('getBeers');
+      });
 
-        cy
-          .get(sel('search-box'))
-          .clear()
-          .type('Burger')
-          .should('have.value', 'Burger')
-          .type('{enter}');
-
-        cy.wait('@getBeers').then(xhr => {
+      describe('WHEN entering text in the search-field', () => {
+        beforeEach(() => {
           cy
-            .wrap(xhr)
-            .its('url')
-            .should('include', '/beers?food=Burger&per_page=80');
-          cy
-            .wrap(xhr)
-            .its('status')
-            .should('eq', 200);
+            .get(sel('search-box'))
+            .clear()
+            .type('Burger')
+            .should('have.value', 'Burger')
+            .type('{enter}');
         });
 
-        cy.url().should('contain', '/#Burger');
+        it('should resolve the request and display a list of beers', () => {
+          cy.wait('@getBeers').then(xhr => {
+            cy
+              .wrap(xhr)
+              .its('url')
+              .should('include', '/beers?food=Burger&per_page=80');
+            cy
+              .wrap(xhr)
+              .its('status')
+              .should('eq', 200);
+          });
 
-        cy.get(sel('beer-card')).should('have.length.above', 0);
+          cy.url().should('contain', '/#Burger');
+
+          cy.get(sel('beer-card')).should('have.length.above', 0);
+        });
       });
     });
 
-    describe('with a stubbed backend', () => {
-      describe('with a slow response', () => {
+    describe('AND a stubbed backend', () => {
+      describe('AND a slow response', () => {
         beforeEach(() => {
           cy.server();
 
@@ -47,21 +53,25 @@ describe('searching for beers', () => {
             response: 'fixture:beers-successful.json',
             delay: 500,
           });
-
-          cy
-            .get(sel('search-box'))
-            .clear()
-            .type('Burger')
-            .should('have.value', 'Burger')
-            .type('{enter}');
         });
 
-        it('should display a loading indicator', () => {
-          cy.get(sel('loading-indicator'));
+        describe('WHEN entering text in the search field', () => {
+          beforeEach(() => {
+            cy
+              .get(sel('search-box'))
+              .clear()
+              .type('Burger')
+              .should('have.value', 'Burger')
+              .type('{enter}');
+          });
+
+          it('should display a loading indicator', () => {
+            cy.get(sel('loading-indicator'));
+          });
         });
       });
 
-      describe('with a successful response', () => {
+      describe('AND a successful response', () => {
         beforeEach(() => {
           cy.server();
 
@@ -70,68 +80,72 @@ describe('searching for beers', () => {
             url: /https:\/\/api.punkapi.com\/v2/,
             response: 'fixture:beers-successful.json',
           });
-
-          cy
-            .get(sel('search-box'))
-            .clear()
-            .type('Burger')
-            .should('have.value', 'Burger')
-            .type('{enter}');
         });
 
-        it('should change the url', () => {
-          cy.url().should('contain', '/#Burger');
-        });
+        describe('WHEN entering text in the search-field', () => {
+          beforeEach(() => {
+            cy
+              .get(sel('search-box'))
+              .clear()
+              .type('Burger')
+              .should('have.value', 'Burger')
+              .type('{enter}');
+          });
 
-        it('should display a list of beers', () => {
-          cy.get(sel('beer-card')).should('have.length', 14);
-        });
+          it('should change the url', () => {
+            cy.url().should('contain', '/#Burger');
+          });
 
-        it('should display complete and correct information on a beer', () => {
-          cy
-            .get(sel('beer-card'))
-            .first()
-            .find(sel('beer-card-name'))
-            .should('contain', 'Libertine Porter');
+          it('should display a list of beers', () => {
+            cy.get(sel('beer-card')).should('have.length', 14);
+          });
 
-          cy
-            .get(sel('beer-card'))
-            .first()
-            .find(sel('beer-card-tagline'))
-            .should('contain', 'Dry-Hopped Aggressive Porter');
+          it('should display complete and correct information on a beer', () => {
+            cy
+              .get(sel('beer-card'))
+              .first()
+              .find(sel('beer-card-name'))
+              .should('contain', 'Libertine Porter');
 
-          cy
-            .get(sel('beer-card'))
-            .first()
-            .find(sel('beer-card-description'))
-            .should(
-              'contain',
-              'An avalanche of cross-continental hop varieties'
-            );
+            cy
+              .get(sel('beer-card'))
+              .first()
+              .find(sel('beer-card-tagline'))
+              .should('contain', 'Dry-Hopped Aggressive Porter');
 
-          cy
-            .get(sel('beer-card'))
-            .first()
-            .find(sel('beer-card-tags'))
-            .should('contain', 'ABV 6.1%')
-            .and('contain', 'IBU 45');
+            cy
+              .get(sel('beer-card'))
+              .first()
+              .find(sel('beer-card-description'))
+              .should(
+                'contain',
+                'An avalanche of cross-continental hop varieties'
+              );
 
-          cy
-            .get(sel('beer-card'))
-            .first()
-            .find(sel('beer-card-food-pairing'))
-            .should('have.length', 3);
+            cy
+              .get(sel('beer-card'))
+              .first()
+              .find(sel('beer-card-tags'))
+              .should('contain', 'ABV 6.1%')
+              .and('contain', 'IBU 45');
 
-          cy
-            .get(sel('beer-card'))
-            .first()
-            .find(sel('beer-card-food-pairing'))
-            .eq(0)
-            .should('contain', 'Blue cheese beef burger');
+            cy
+              .get(sel('beer-card'))
+              .first()
+              .find(sel('beer-card-food-pairing'))
+              .should('have.length', 3);
+
+            cy
+              .get(sel('beer-card'))
+              .first()
+              .find(sel('beer-card-food-pairing'))
+              .eq(0)
+              .should('contain', 'Blue cheese beef burger');
+          });
         });
       });
 
-      describe('with an error-response', () => {
+      describe('AND an error-response', () => {
         beforeEach(() => {
           cy.server();
 
@@ -143,21 +157,25 @@ describe('searching for beers', () => {
             },
             status: 503,
           });
-
-          cy
-            .get(sel('search-box'))
-            .clear()
-            .type('Burger')
-            .should('have.value', 'Burger')
-            .type('{enter}');
         });
 
-        it('should change the url', () => {
-          cy.url().should('contain', '/#Burger');
-        });
+        describe('WHEN entering text in the search-field', () => {
+          beforeEach(() => {
+            cy
+              .get(sel('search-box'))
+              .clear()
+              .type('Burger')
+              .should('have.value', 'Burger')
+              .type('{enter}');
+          });
 
-        it('should display an error-message', () => {
-          cy.get(sel('error-message'));
+          it('should change the url', () => {
+            cy.url().should('contain', '/#Burger');
+          });
+
+          it('should display an error-message', () => {
+            cy.get(sel('error-message'));
+          });
         });
       });
     });
